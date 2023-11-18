a88951efd46663a23030abebdb1dc0439fe2a588
Dmitrii Parfenov <parfenovds@protonmail.com>
2023-11-18 15:15:02 +0300

    add test for test

---
+++ src/test/java/com/javarush/parfenov/util/ForTestingPurposeTest.java
@@ -0,0 +1,11 @@
+package com.javarush.parfenov.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ForTestingPurposeTest {
+  @Test
+  void testForTest() {
+    assertEquals(1, 1);
+  }
+}
